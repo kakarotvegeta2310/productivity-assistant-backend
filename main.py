baf1f43d9b49951fc98197c5635f1110593baa0b
Vishwa <kakarotvegeta4317@gmail.com>
2026-05-09 13:12:37 +0530

    update CORS

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
 )
 allow_origins=[
     "http://localhost:3000",
-    "https://your-app.vercel.app"
+    "https://productivity-assistant-frontend-6bo.vercel.app"
 ],
 app.include_router(assistant_router)
 
